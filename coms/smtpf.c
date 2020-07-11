598398d4ee62c058bafd00dc7f57d22a81a153eb
Ryan Bruno <ryan@rbruno.com>
2020-07-11 12:22:57 -0400

    SMTPf

--- coms/smtpf.c
+++ coms/smtpf.c
@@ -39,10 +39,9 @@
         if (*z == '\n') \
             break; }}
 
-char* local_host = "0.0.0.0";
 char* greet = "HELO flappy.rbruno.com\r\n";
 int greet_len = 24;
-char* port = "2525";
+char* port = "25";
 
 struct addrinfo ai_hints = {
     .ai_flags    = AI_NUMERICSERV,
@@ -171,7 +170,7 @@ int main(int argc, char *argv[])
             case '\0':
                 return -1;
             case '\n':
-                if (h == NULL) rcpt_set[rcpt_set_i++] = local_host;
+                if (h == NULL) rcpt_set_i--;
                 else rcpt_set[rcpt_set_i++] = h;
                 *e = '\0';
                 data = e + 1;
@@ -182,7 +181,7 @@ int main(int argc, char *argv[])
                 h = e + 1;
                 break;
             case ',':
-                if (h == NULL) rcpt_set[rcpt_set_i++] = local_host;
+                if (h == NULL) rcpt_set_i--;
                 else rcpt_set[rcpt_set_i++] = h;
                 rcpt_set[rcpt_set_i++] = e + 1;
                 *e = '\0';
@@ -191,6 +190,8 @@ int main(int argc, char *argv[])
         }
         if (e == NULL) break;
     }
+    printf("%d\n", rcpt_set_i);
+    for (int i = 0; i < rcpt_set_i; i++) printf("%s\n", rcpt_set[i]);
 
     for (char** r = rcpt_set + 2; rcpt_set_i > 2; r += 2, rcpt_set_i -= 2) {
         struct addrinfo *a;
@@ -198,9 +199,9 @@ int main(int argc, char *argv[])
         if (r[0] == NULL) continue;
 
         /* Socket Setup */
-        /*if (resolve_first_mx(r[1], target_mx_host, sizeof(target_mx_host)) < 0 ||
-            getaddrinfo(target_mx_host, port, &ai_hints, &a) != 0 || */
-        if (getaddrinfo("127.0.0.1", port, &ai_hints, &a) != 0 ||
+        if (resolve_first_mx(r[1], target_mx_host, sizeof(target_mx_host)) < 0 ||
+            getaddrinfo(target_mx_host, port, &ai_hints, &a) != 0 ||
+        //if (getaddrinfo("127.0.0.1", port, &ai_hints, &a) != 0 ||
             (fd = socket(a->ai_family, a->ai_socktype, a->ai_protocol)) == -1 ||
             connect(fd, a->ai_addr, a->ai_addrlen) == -1)
         {
@@ -226,10 +227,10 @@ int main(int argc, char *argv[])
         if (read_reply_code(fd, data_buf, sizeof(data_buf)) != 250) goto HOLD_ALL;
         RUN_OFF(data_buf);
 
-        for (int i = 0; i < rcpt_set_i; i += 2) {
+        for (int i = r - rcpt_set; i < rcpt_set_i; i += 2) {
             if (r[i] == NULL) continue;
-            if (strcmp(r[1], r[i + 1]) == 0) {
-                SEND("RCPT TO:<%s>\r\n", r[i]);
+            if (strcmp(r[1], rcpt_set[i + 1]) == 0) {
+                SEND("RCPT TO:<%s>\r\n", rcpt_set[i]);
 
                 /* Bounce */
                 if (read_reply_code(fd, data_buf, sizeof(data_buf)) != 250)
