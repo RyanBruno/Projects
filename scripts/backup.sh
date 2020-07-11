22acc262e91cd71ef5de74101f5973bd6fe1b3d3
Ryan Bruno <ryan@rbruno.com>
2020-07-11 13:30:26 -0400

    Backup changes

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-tar czvf /home/object/Objects/Backups/$(date +%Y.%m.%d).tar.gz /home/ryan/.gnupg/ /home/vault/.gnupg/ /home/vault/Private/ /home/ryan/TheRepository/ /home/ryan/.ssh/ /home/ryan/ABS/ /home/object/Pending && rm -r /home/object/Pending/*
+tar czvf /home/object/Objects/Backups/$(date +%Y.%m.%d).tar.gz /home/ryan/.gnupg/ /home/vault/.gnupg/ /home/vault/Private/ /home/ryan/TheRepository/ /home/ryan/.ssh/
 chown -R object:object /home/object/
