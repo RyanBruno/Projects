a6854168395affb0f48c27357e18634fe6f7240d
Ryan Bruno <ryan@rbruno.com>
2021-08-08 18:43:34 -0400

    setup.sh fixes

--- pkg/setup.sh
+++ pkg/setup.sh
@@ -1,11 +1,15 @@
 
-# TODO join overlay01
+# Join overlay01
+sudo pacman-key --init
+sudo pacman -Syu wireguard-tools
+sudo wg-quick up overlay01.temp.conf
+
 
 KEYID="915EBB9C9889E4AEB983CD96F15F942AA25D5A6A"
 pacman-key --recv-key $KEYID --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key $KEYID
 
-CONF=$(cat <<EOF
+cat <<EOF > /tmp/pacman.conf.ryan
 [options]
 HoldPkg     = pacman glibc
 Architecture = auto
@@ -19,16 +23,16 @@ LocalFileSigLevel = Optional
 Server = http://10.0.1.1/pkg/
 
 [core]
-Server = http://10.0.1.1/archlinux/$repo/os/$arch
+Server = http://10.0.1.1/archlinux/\$repo/os/\$arch
 
 [extra]
-Server = http://10.0.1.1/archlinux/$repo/os/$arch
+Server = http://10.0.1.1/archlinux/\$repo/os/\$arch
 
 #[community-testing]
 #Include = /etc/pacman.d/mirrorlist
 
 [community]
-Server = http://10.0.1.1/archlinux/$repo/os/$arch
+Server = http://10.0.1.1/archlinux/\$repo/os/\$arch
 
 # If you want to run 32 bit applications on your x86_64 system,
 # enable the multilib repositories as required here.
@@ -37,7 +41,7 @@ Server = http://10.0.1.1/archlinux/$repo/os/$arch
 #Include = /etc/pacman.d/mirrorlist
 
 [multilib]
-Server = http://10.0.1.1/archlinux/$repo/os/$arch
+Server = http://10.0.1.1/archlinux/\$repo/os/\$arch
 
 # An example of a custom package repository.  See the pacman manpage for
 # tips on creating your own repositories.
@@ -45,9 +49,7 @@ Server = http://10.0.1.1/archlinux/$repo/os/$arch
 #SigLevel = Optional TrustAll
 #Server = file:///home/custompkgs
 EOF
-)
 
-echo 'CONF' > /tmp/pacman.conf.ryan
 
 pacman --config /tmp/pacman.conf.ryan -Syu pacman.ryan
 pacman.ryan -Syu base.ryan
